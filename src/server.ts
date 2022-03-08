6df3fa91a332cfead8404cd448a9dd4ae4f4852e
Abraham Hernandez <abeherham@knights.ucf.edu>
2022-03-08 00:37:08 -0500

    Cleaned up repo and migrated from mongodb to mongoose.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
+/* eslint-disable no-console */
+
 import express from "express";
 const app = express();
+import "dotenv/config";
+
+import connectDB from "./db";
 const port = process.env.PORT || 8080; // Listening port defaults to 8080 if there's no env port
+
+connectDB();
 
 // Define route handler for the default home page
 app.get("/api", (req, res) => {
@@ -17,5 +24,5 @@ app.get("/api/testendpoint", (req, res) => {
 
 // Start express server
 app.listen(port, () => {
-  console.log(`server started at http://localhost${port}`);
+  console.log(`server started at http://localhost:${port}`);
 });
