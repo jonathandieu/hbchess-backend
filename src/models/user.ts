707af0bf19cc340c7da68e9fb9b1a887101baee8
Phyremaster <phyremaster@hotmail.com>
2022-03-08 21:29:59 -0500

    Cleaned up constraints

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,17 +5,16 @@ const UserSchema = new Schema({
   email: {
     type: String,
     trim: true,
-    lowercase: true,
     unique: true,
     required: [true, "Email address is required"],
     match: [
-      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      /^.+@.+\..+$/,
       "Please fill a valid email address"
     ]
   },
-  username: { type: String, required: true, unique: true, index: true },
+  username: { type: String, required: true, trim: true, unique: true },
   password: { type: String, required: true },
-  emailToken: { type: String, required: true, unique: true },
+  emailToken: { type: String, required: true },
   isVerified: { type: Boolean, default: false }
 });
 
