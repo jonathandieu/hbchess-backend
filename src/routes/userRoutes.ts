bb82f29e26dc00e2337f3fb7ea98878e851a58ee
asachs111 <asachs111@knights.ucf.edu>
2022-03-09 14:58:14 -0500

    Added VerifyEmail to the routes file

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController";
+import {
+  loginUser,
+  registerUser,
+  verifyEmail
+} from "../controllers/userController";
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/verifyEmail", verifyEmail);
 router.post("/login", loginUser);
 
 export default router;
