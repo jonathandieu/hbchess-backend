97520e100f75e4623ad7cdd1f31deb4c61003e27
Tye <iwatchtyler@gmail.com>
2022-03-19 01:31:08 -0400

    Fixed for API

--- src/controllers/teamController.ts
+++
@@ -1,8 +0,0 @@
-import asyncHandler from "express-async-handler";
-import Team, { ITeam } from "../models/team";
-import User, { IUser } from "../models/user";
-import "dotenv/config";
-
-export const createTeam = asyncHandler(async (req, res) => {
-  const user = req.User;
-});
