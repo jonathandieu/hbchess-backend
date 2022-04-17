4ed956e71c6d3cdefa8ce4a5b54dce399d26b153
Tye <iwatchtyler@gmail.com>
2022-04-16 23:49:49 -0400

    team bug

--- src/models/team.ts
+++ src/models/team.ts
@@ -27,9 +27,9 @@ export const TeamSchema = new Schema({
     ]
   },
   matches: { type: Number, required: true, default: 0 },
-  wins: { type: [mongoose.Types.ObjectId], required: true, default: 0 },
-  losses: { type: [mongoose.Types.ObjectId], required: true, default: 0 },
-  ties: { type: [mongoose.Types.ObjectId], required: true, default: 0 },
+  wins: { type: [mongoose.Types.ObjectId], required: true, default: [] },
+  losses: { type: [mongoose.Types.ObjectId], required: true, default: [] },
+  ties: { type: [mongoose.Types.ObjectId], required: true, default: [] },
   name: { type: String, required: true, default: "Everything is Team Name" },
   accepted: { type: Boolean, required: true, default: false }
 });
